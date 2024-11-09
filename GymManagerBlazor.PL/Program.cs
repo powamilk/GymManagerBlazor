ef23a15c17e87c4744f4d31c5de08317fb7e528a
Hiệu burando Tô <hieutotoka@gmail.com>
2024-11-09 22:56:37 +0700

    Su dung mudblazor

--- GymManagerBlazor.PL/Program.cs
+++ GymManagerBlazor.PL/Program.cs
@@ -11,6 +11,7 @@ using GymManagerBlazor.PL;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -30,10 +31,12 @@ builder.Services.AddTransient<IValidator<ClassModel>, ClassModelValidator>();
 builder.Services.AddTransient<IValidator<MemberModel>, MemberModelValidator>();
 builder.Services.AddTransient<IValidator<ClassRegistrationModel>, ClassRegistrationModelValidator>();
 
+
 builder.Services.AddScoped<IClassService, ClassService>();
 builder.Services.AddScoped<IMemberService, MemberService>();
 builder.Services.AddScoped<ITrainerService, TrainerService>();
 builder.Services.AddScoped<IClassRegistrationService, ClassRegistrationService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
